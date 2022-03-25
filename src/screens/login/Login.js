54db9896a809b8d3867eaf0954944aed30ae2396
Paulo Cesar Cuneo <cuneo.paulo.cesar@gmail.com>
2022-03-25 12:31:06 -0300

    2 Connect Login

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,27 +1,61 @@
 import { useState } from "react";
+import { login } from "./service"
 
 function Login() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const doLogin = (_) => console.log({ email, password })
-    const updatePassword = (e) => setPassword(e.target.value)
+    const [error, setError] = useState('')
+    const [token, setToken] = useState('')
+    const [loading, setLoading] = useState(false)
+    const doLogin = async (_) => {
+        setLoading(true)
+        try {
+            const { token } = await login({ email, password })
+            setToken(token)
+        } catch (e) {
+            setError(e.message)
+        }
+        setLoading(false)
+    }
+    const updateEmail = (e) => {
+        setEmail(e.target.value)
+        setError('')
+    }
+    const updatePassword = (e) => {
+        setPassword(e.target.value)
+        setError('')
+    }
     return (
         <div>
             <h1>Users</h1>
             <p>See other users profiles</p>
+            {token &&
+                <div style={{ backgroundColor: 'green' }}>
+                    Succesful login, token: {token}
+                </div>
+            }
+            <div style={{ backgroundColor: 'red' }}>
+                {error}
+            </div>
+            {loading && <div style={{ backgroundColor: 'yellow' }}>
+                Loading ...
+            </div>}
             <div>
                 <div>
                     <span>email:</span>
                     <input type="text"
-                        onChange={(e) => setEmail(e.target.value)} />
+                        onChange={updateEmail}
+                        disabled={loading} />
                 </div>
                 <div>
                     <span>password:</span>
                     <input type="password"
-                        onChange={updatePassword} />
+                        onChange={updatePassword}
+                        disabled={loading} />
                 </div>
                 <div>
-                    <button onClick={doLogin}>
+                    <button onClick={doLogin}
+                        disabled={loading}>
                         Log In
                     </button>
                 </div>
---
+++ src/screens/login/service.js
@@ -0,0 +1,22 @@
+
+export async function login(credentials) {
+    const res = await fetch('https://reqres.in/api/login', {
+        method: 'POST',
+        body: JSON.stringify(credentials),
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    })
+    let json = null
+    try {
+        json = await res.json()
+    } catch (e) {
+        console.error(e.message ||'error parsing json')
+        throw new Error('Service is not available at the moment')
+    }
+    if (!res.ok) {
+        throw new Error(json.error)
+    }
+    return json
+}
