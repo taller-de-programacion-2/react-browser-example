f552bcc23da7f761e103c9106d8e6fcf5eaf5a3a
Paulo Cesar Cuneo <cuneo.paulo.cesar@gmail.com>
2022-03-27 20:17:12 -0300

    Use localstorage to remember token

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import login from "./service"
 
-const Login = () => {
+const Login = ({onLogin}) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [error, setError] = useState('')
