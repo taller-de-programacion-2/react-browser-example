286ef431f4c206424594b93037e0c17a4c8f8574
rodrigo echeconea <rodrigoecheconea@ip-192-168-0-204.ec2.internal>
2022-04-14 11:24:33 -0300

    added axios, changed everything to arrow

--- src/contexts/auth/service.js
+++ src/contexts/auth/service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const login = async (credentials) => {
+const login = async (credentials) => {
     try {
         const response = await axios.post(
             `${process.env.REACT_APP_API_URL}/api/login`,
