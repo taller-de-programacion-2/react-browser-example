cc7fa7d45219e5a8dd3269aad7d53cb10c76889f
Paulo Cesar Cuneo <cuneo.paulo.cesar@gmail.com>
2022-05-07 14:43:35 -0300

    useCallback

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -5,13 +5,19 @@ import Listing from "../../components/listing/Listing";
 
 
 function Home() {
-    const session = useSession()
+    const session = useSession();
 
-    const UserPages = () =>
-        <Listing fetchPage={async (page) => {
-            const { data, total_pages } = await listUsers(page)
-            return { data, total: total_pages };
-        }} />;
+    const UserCard = ({ item }) => (
+        <div>
+            {item.first_name} / {item.email}
+            <img src={item.avatar}
+                alt={item.email} />
+        </div>);
+
+    const listUserAdapter = async (page) => {
+        const { data, total_pages } = await listUsers(page)
+        return { data, total: total_pages };
+    }
 
     useEffect(() => {
         return () => {
@@ -30,7 +36,10 @@ function Home() {
                     <button onClick={session.logout}>Log out</button>
                 </span>
             </div>
-            <UserPages />
+            <Listing
+                ItemComponent={UserCard}
+                fetchPage={listUserAdapter}
+            />
         </div>
     );
 }
