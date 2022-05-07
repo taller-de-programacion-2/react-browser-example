a0f28d9a4dd268521be53b187477aac49a36e5ce
Paulo Cesar Cuneo <cuneo.paulo.cesar@gmail.com>
2022-05-07 14:43:35 -0300

    useCallback

--- src/components/listing/Listing.js
+++ src/components/listing/Listing.js
@@ -1,28 +1,30 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-const Listing = ({ fetchPage }) => {
+const Listing = ({ fetchPage, ItemComponent }) => {
     const [data, setData] = useState([]);
     const [page, setPage] = useState(1);
     const [total, setTotal] = useState(1);
-    const doFetchPage = async (page) => {
+
+    const doFetchPage = useCallback(async (page) => {
         const { data, total } = await fetchPage(page);
         setTotal(total);
         setData(data);
-    };
+    }, [fetchPage]);
+
     const nextPage = () => setPage(page + 1);
     const prevPage = () => setPage(page - 1);
 
     useEffect(() => {
         console.log(`fetching ${page}`);
         doFetchPage(page);
-    }, [page]);
+    }, [page, doFetchPage]);
 
     return <div>
         <ul>
-            {data.map(user => <li key={user.id}>
-                {user.first_name} / {user.email}
-                <img src={user.avatar} alt={user.email} />
-            </li>)
+            {data.map(item =>
+                <li key={item.id}>
+                    <ItemComponent item={item} />
+                </li>)
             }
         </ul>
         <button onClick={prevPage} disabled={page === 1}>
