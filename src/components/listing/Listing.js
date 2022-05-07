14bb8c6f99a6cbf3588d48a70b30220523adec8c
Paulo Cesar Cuneo <cuneo.paulo.cesar@gmail.com>
2022-05-07 14:24:06 -0300

    Extract component

---
+++ src/components/listing/Listing.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+const Listing = ({ fetchPage }) => {
+    const [data, setData] = useState([]);
+    const [page, setPage] = useState(1);
+    const [total, setTotal] = useState(1);
+    const doFetchPage = async (page) => {
+        const { data, total } = await fetchPage(page);
+        setTotal(total);
+        setData(data);
+    };
+    const nextPage = () => setPage(page + 1);
+    const prevPage = () => setPage(page - 1);
+    useEffect(() => {
+        console.log(`fetching ${page}`);
+        doFetchPage(page);
+    }, [page]);
+    return <div>
+        <ul>
+            {data.map(user => <li key={user.id}>
+                {user.first_name} / {user.email}
+                <img src={user.avatar} alt={user.email} />
+            </li>)
+            }
+        </ul>
+        <button onClick={prevPage} disabled={page === 1}>
+            prev
+        </button>
+        {page}
+        <button onClick={nextPage} disabled={page === total}>
+            next
+        </button>
+    </div>
+}
+export default Listing;
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,44 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { listUsers } from './service'
-import {useSession} from '../../contexts/auth/Auth';
+import { useSession } from '../../contexts/auth/Auth';
+import Listing from "../../components/listing/Listing";
 
 
 function Home() {
     const session = useSession()
-    const [users, setUsers] = useState([]);
-    const [page, setPage] = useState(1);
-    const [total, setTotal] = useState(1);
 
-    const fetchPage = async (page) => {
-        const { data, total_pages } = await listUsers(page);
-        setTotal(total_pages);
-        setUsers(data);
-    };
-
-    const UserPages = () => {
-        const nextPage = () => setPage(page + 1);
-        const prevPage = () => setPage(page - 1);
-        return <div>
-            <ul>
-                {users.map(user => <li key={user.id}>
-                    {user.first_name} / {user.email}
-                    <img src={user.avatar} alt={user.email}/>
-                </li>)
-                }
-            </ul>
-            <button onClick={prevPage} disabled={page === 1}>
-                prev
-            </button>
-            {page}
-            <button onClick={nextPage} disabled={page === total}>
-                next
-            </button>
-        </div>
-    }
-    useEffect(() => {
-        console.log(`fetching ${page}`);
-        fetchPage(page);
-    }, [page]);
+    const UserPages = () =>
+        <Listing fetchPage={async (page) => {
+            const { data, total_pages } = await listUsers(page)
+            return { data, total: total_pages };
+        }} />;
 
     useEffect(() => {
         return () => {
