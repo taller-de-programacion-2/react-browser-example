47cd4eccea8dce2dbd7fb43de81a0caefb2421b1
rodrigo echeconea <rodrigoecheconea@ip-192-168-0-204.ec2.internal>
2022-05-11 20:38:27 -0300

    small refactors

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { listUsers } from './service'
+import usersService from '../../services/users'
 import { useSession } from '../../contexts/auth/Auth';
 import Listing from "../../components/listing/Listing";
 
@@ -9,7 +9,7 @@ function Home() {
 
     const UserPages = () =>
         <Listing fetchPage={async (page) => {
-            const { data, total_pages } = await listUsers(page)
+            const { data, total_pages } = await usersService.listUsers(page)
             return { data, total: total_pages };
         }} />;
 
